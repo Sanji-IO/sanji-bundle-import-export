2ac6cc2afbb365e50db53c6360f08bb77cfaa2d9
ZarZar <ckgczarzar@gmail.com>
2015-02-06 16:51:28 +0800

    Commit 2 unfinished code because I have no time to finish these. Sorry.

--- import_export.py
+++ import_export.py
@@ -28,10 +28,11 @@ class ImportExport(Sanji):
 
     @Route(methods="get", resource="/system/importexport")
     def get(self, message, response):
-        # export_config
-        self.collect_file(self.search_root, self.db_postfix,
-                          self.db_folder_name)
-        #self.exclude_file()
+        # export configuration file.
+        self.collect_file(self.search_root, self.db_folder_name,
+                          self.db_postfix,
+                          )
+        # self.exclude_file()
         if os.path.isfile(self.export_file):
             return response(code=200, data="/system/importexport/download")
         else:
@@ -39,6 +40,7 @@ class ImportExport(Sanji):
 
     @Route(methods="put", resource="/system/importexport")
     def put(self, message, response):
+        # import configuration file.
         if self.restore_file() is True:
             return response(code=200, data="/system/importexport/download")
         else:
@@ -62,14 +64,15 @@ class ImportExport(Sanji):
 
     def collect_file(self, start_root, dir_keyword,
                      postfix, unique=False):
+
         # search
         for root, dirs, files in os.walk(start_root):
-            for dir in dirs:
-                for file in files:
-                    #print root, dir, file
-                    if file.endswith(postfix) and dir == dir_keyword:
-                        source_file = os.path.join(root, file)
-                        #tar.add(os.path.abspath(source_file))
+            for file in files:
+                if file.endswith(postfix):
+                    source_file = os.path.join(root, file)
+                    split_list = source_file.split("/")
+                    if split_list[-2] == dir_keyword:
+                        #print "ADD: ", os.path.abspath(source_file)
                         self.import_set.add(os.path.abspath(source_file))
                         if unique is True:
                             break
--- tests/test_import_export.py
+++ tests/test_import_export.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-
 import os
 import sys
+import shutil
 import unittest
 import logging
 
 from sanji.connection.mockup import Mockup
 from sanji.message import Message
 from mock import patch
+from mock import Mock
 
 logger = logging.getLogger()
 
@@ -21,32 +22,43 @@ except ImportError as e:
         % __file__
     exit(1)
 
+export_file = "/run/shm/export.tar"
 
-class TestSshClass(unittest.TestCase):
+class TestImportExportClass(unittest.TestCase):
+    file_folder = "/run/shm/test_import_export"
+    file_name = "/run/shm/test_import_export/export.tar"
 
     def setUp(self):
-        self.import_export = ImportExport(connection=Mockup())
+        if os.path.isfile(self.file_name) is True:
+            os.remove(self.file_name)
+        self.ie = ImportExport(connection=Mockup())
 
     def tearDown(self):
-        self.import_export = None
+        if os.path.isfile(self.file_name):
+            os.remove(self.file_name)
+        if os.path.isdir(self.file_folder):
+            shutil.rmtree(self.file_folder)
+
+        self.ie = None
 
     def test_init(self):
+        self.assertEquals(self.ie.model.model_name, "import_export")
 
-        # case 1: ssh start failed
-
-        # case 2: ssh start success
-
-
+    @patch("import_export.os.path.isfile")
     def test_get(self):
-        # case 1: check code
-
-
-        # case 2: check data of enable = 1
-
-
-        # case 3: check data of enable = 0
-
-
+        # case 1: check code 200
+        def resp(code=200, data=None):
+            self.assertEqual(code, 200)
+        self.ie.get(message=None, response=resp, test=True)
+        self.assertTrue(os.path.isfile(export_file))
+        '''
+        # case 2: check code 400
+        def resp(code=200, data=None):
+            self.assertEqual(code, 400)
+        with patch("os.path.isfile") as os_path_isfile:
+            os_path_isfile.return_value = "False"
+            self.ie.get(message=None, response=resp, test=True)
+        '''
     def test_put(self):
         # case 1: message donsn't has data attribute
 
@@ -60,7 +72,48 @@ class TestSshClass(unittest.TestCase):
 
         # case 3: put failed
         # case 3.1: ssh start failed
+        pass
 
+    def test_collect_file(self):
+        if not os.path.exists(self.file_folder):
+            os.makedirs(self.file_folder)
+            os.makedirs(self.file_folder + "/bundle_a/data")
+            os.makedirs(self.file_folder + "/bundle_b/data")
+            os.makedirs(self.file_folder + "/bundle_c/non_data")
+        with open("/run/shm/test_import_export/bundle_a/data/a.json",
+                  "w") as file:
+            file.write("This is a file.")
+        with open("/run/shm/test_import_export/bundle_b/data/b.json",
+                  "w") as file:
+            file.write("This is b file.")
+        with open("/run/shm/test_import_export/bundle_c/non_data/non_c.json",
+                  "w") as file:
+            file.write("This is non_c file.")
+
+        self.ie.collect_file("/run/shm/test_import_export", "data", ".json")
+        self.assertEqual(self.ie.import_set,
+                         set(["/run/shm/test_import_export/\
+bundle_a/data/a.json",
+                             "/run/shm/test_import_export/\
+bundle_b/data/b.json"]))
+
+    def test_tar_files(self):
+
+        if not os.path.exists(self.file_folder):
+            os.makedirs(self.file_folder)
+        with open("/run/shm/test_import_export/a.txt", "w") as file:
+            file.write("This is a file.")
+        with open("/run/shm/test_import_export/b.txt", "w") as file:
+            file.write("B B BBB")
+        self.ie.import_set.add("/run/shm/test_import_export/a.txt")
+        self.ie.import_set.add("/run/shm/test_import_export/b.txt")
+
+        self.ie.export_folder = self.file_folder
+        self.ie.export_file = self.file_name
+        self.ie.tar_files()
+
+        # case 1: check export.tar
+        self.assertTrue(os.path.isfile(self.file_name))
 
 if __name__ == "__main__":
     unittest.main()
